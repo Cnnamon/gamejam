f2db586fe6061f7d80eb4affc62810a70acf808e
AroSaneD <mail.junk.3135@gmail.com>
2015-01-24 00:06:23 +0200

    hey guys

---
+++ desktop/src/epic/platformer/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package epic.platformer.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import epic.platformer.game.Platformer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Platformer(), config);
+	}
+}
