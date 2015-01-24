75e6d2f7a3d9dd1539a5f4b99ad0a9fc84dbea0c
Laurynas <laurynas@lazauskas.info>
2015-01-24 04:22:51 +0200

    First attempt at CollisionObject

---
+++ core/src/epic/platformer/game/CollisionObject.java
@@ -0,0 +1,22 @@
+package epic.platformer.game;
+
+/**
+ * Created by laurynas on 2015.01.24.
+ */
+import com.badlogic.gdx.math.Rectangle;
+
+public class CollisionObject extends Rectangle {
+    int group;
+
+    public CollisionObject()
+    {
+        super(0, 0, 0, 0);
+    }
+    public CollisionObject(float x, float y, float w, float h)
+    {
+        super(x, y, w, h);
+    }
+
+    public int getGroup() { return group; }
+    public void setGroup(int group) { this.group = group; }
+}
