8c94339407161eaebbf6cceb1afbb516695d2db1
Dominykas Vilimas <dominykas.vilimas@gmail.com>
2015-01-25 13:07:17 +0200

    jou

--- core/src/screens/MenuScreen.java
+++ core/src/screens/MenuScreen.java
@@ -8,11 +8,14 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import epic.platformer.game.Assets;
 import epic.platformer.game.GameScreen;
@@ -35,6 +38,11 @@ public class MenuScreen implements Screen {
 
     Label playButton;
     float playButtonScale = 5;
+    TextButton playTextButton;
+    TextButton.TextButtonStyle playTextButtonStyle;
+    BitmapFont font;
+    Skin skin;
+    TextureAtlas buttonAtlas;
 
     public MenuScreen(Platformer game) {
         this.game = game;
@@ -49,16 +57,29 @@ public class MenuScreen implements Screen {
         this.camera.setToOrtho(false, Assets.screenSizeWidth, Assets.screenSizeHeight);
         stage.getViewport().setCamera(camera);
         Gdx.input.setInputProcessor(stage);
-        playButton.addListener(playButtonClickListener = new ClickListener(){
+
+        skin = new Skin();
+        buttonAtlas = new TextureAtlas(Gdx.files.internal("mainButtons.pack"));
+        skin.addRegions(buttonAtlas);
+        playTextButtonStyle = new TextButton.TextButtonStyle();
+        playTextButtonStyle.font = new BitmapFont();
+        playTextButtonStyle.up = skin.getDrawable("Play");
+        playTextButtonStyle.down = skin.getDrawable("PLayP");
+        playTextButtonStyle.checked = skin.getDrawable("PLayP");
+        playTextButton = new TextButton(null, playTextButtonStyle);
+        stage.addActor(playTextButton);
+//        stage.addActor(playButton);
+
+        playTextButton.addListener(playButtonClickListener = new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 game.setScreen(new GameScreen(game));
-                playButton.removeListener(playButtonClickListener);
+                playTextButton.removeListener(playButtonClickListener);
                 super.clicked(event, x, y);
             }
         });
 
-        stage.addActor(playButton);
+
 
         Sounds.playMenuMusic();
 
@@ -68,8 +89,8 @@ public class MenuScreen implements Screen {
     public void render(float delta) {
 
 
-//        Gdx.graphics.getGL20().glClearColor( 1, 0, 0, 1 );
-//        Gdx.graphics.getGL20().glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );
+        Gdx.graphics.getGL20().glClearColor( 1, 1, 1, 1 );
+        Gdx.graphics.getGL20().glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );
 
 //        if(Gdx.input.isKeyPressed(Input.Keys.ANY_KEY)) {
 //            game.setScreen(new GameScreen(game));
@@ -82,10 +103,10 @@ public class MenuScreen implements Screen {
         stage.draw();
         game.batch.begin();
             game.batch.setColor(Color.WHITE);
-            game.batch.draw(Assets.textureBack, 0, 0);
+//            game.batch.draw(Assets.textureBack, 0, 0);
 //          playButton.setFontScale(playButtonScale);
-            playButton.setPosition(Assets.screenSizeWidth/2-playButton.getWidth()/2, Assets.screenSizeHeight/2-playButton.getHeight()/2);
-            playButton.draw(game.batch, 1f);
+            playTextButton.setPosition(Assets.screenSizeWidth/2-playTextButton.getWidth()/2, Assets.screenSizeHeight/2-playTextButton.getHeight()/2);
+//            playButton.draw(game.batch, 1f);
 
 
 //        font.setColor(1.0f, 1.0f, 1.0f, 1.0f);
